503cdfdf618a257f00eab596cf7af9cf7bd92a50
devzone-creator <munkailasule3@gmail.com>
2025-06-19 20:44:31 +0000

    fixer

--- pdf2docx_convert.py
+++ pdf2docx_convert.py
@@ -8,6 +8,8 @@ if len(sys.argv) != 3:
 pdf_path = sys.argv[1]
 docx_path = sys.argv[2]
 
+print("Converting:", pdf_path, "to", docx_path)
+
 cv = Converter(pdf_path)
 cv.convert(docx_path, start=0, end=None)
 cv.close()
--- pdf2txt.py
+++ pdf2txt.py
@@ -8,6 +8,8 @@ if len(sys.argv) != 3:
 pdf_path = sys.argv[1]
 txt_path = sys.argv[2]
 
+print("Converting:", pdf_path, "to", txt_path)
+
 text = extract_text(pdf_path)
 with open(txt_path, "w", encoding="utf-8") as f:
     f.write(text)
